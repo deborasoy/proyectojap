834091f26480754a7881efb8af7d51b46f4f7f73
Debora Peña <debo4978@gmail.com>
2023-09-05 22:22:58 -0300

    mejorando codigo

--- js/product-info.js
+++ js/product-info.js
@@ -3,7 +3,7 @@
 const prodCatId = localStorage.getItem("prodCatID");
 const URLinfo = `https://japceibal.github.io/emercado-api/products/${prodCatId}.json `
 const URLcomment = `https://japceibal.github.io/emercado-api/products_comments/${prodCatId}.json`
-let infoProduct // array con data global
+let infoProduct = []; // array con data global
 let comments // array global
 const container = document.getElementById("cont");
 
@@ -12,12 +12,11 @@ async function getInfoProduct() {
     const response = await fetch(URLinfo);
     if (!response.ok) throw new Error(`Code error:${response.status}`)
     const data = await response.json();
-    console.log(data);
-    //mejorar este detalle: obtener solamente el valor de la promesa con los datos que voy a utilizar en la funcion 
-
-    const {name, currency,cost, description, category, soldCount,images} = data;
-    
-    showInfoProduct(name, currency,cost, description,category, soldCount,images)//que la funcion tenga un array u objeto de parametro
+    //console.log(data)
+    const { name, category, cost, currency, description, images } = data;
+    infoProduct = data;
+    console.log(infoProduct)
+    showInfoProduct(infoProduct);
 
    
 }
@@ -26,31 +25,30 @@ async function getCommentsProd() {
     const response = await fetch(URLcomment);
     if (!response.ok) throw new Error(`Code error:${response.status}`);
     const data = await response.json();
-    console.log(data);
+    //console.log(data);
     
 
 }
 // presentarla en product-info.html
-//mejorar codigo
-function showInfoProduct(nam, curr, cost, description, cat, soldCount, imgs) {
+function showInfoProduct(objet) {
     
      let template = 
             `
             <div>
-              <h2 style="text-align:center" >${nam}</h2>
+              <h2 style="text-align:center" >${objet.name}</h2>
             </div>
             <div class="card-body">
-             <p>Precio: <br> ${curr} ${cost}</p>
-             <p>Descripcion <br>${description}</p>
-             <p>Categoria <br>${cat}<p>
-             <p>vendidos:${soldCount}<p>
+             <p>Precio: <br> ${objet.currency} ${objet.cost}</p>
+             <p>Descripcion <br>${objet.description}</p>
+             <p>Categoria <br>${objet.category}<p>
+             <p>vendidos:${objet.soldCount}<p>
             </div>
             <div class="card" >
             <p>Imagenes Ilustrativas</p>
-            <img style="width:250px" src="${imgs[0]}" alt="Card image cap">
-            <img style="width:250px"  src="${imgs[1]}" alt="Card image cap">
-            <img style="width:250px" src="${imgs[2]}" alt="Card image cap">
-            <img style="width:250px"  src="${imgs[3]}" alt="Card image cap">
+            <img style="width:250px" src="${objet.images[0]}" alt="Card image cap">
+            <img style="width:250px"  src="${objet.images[1]}" alt="Card image cap">
+            <img style="width:250px" src="${objet.images[2]}" alt="Card image cap">
+            <img style="width:250px"  src="${objet.images[3]}" alt="Card image cap">
             </div>
         `;
     container.innerHTML = template;
